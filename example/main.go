d509e73c5a0c7b04200e67852c146f7c2c0d26dc
Hubby <hubby@rmhubbert.com>
2026-01-18 16:01:12 +0000

    docs: update example app

--- example/main.go
+++ example/main.go
@@ -2,23 +2,33 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/goforj/godump"
 	"github.com/rmhubbert/tuifade"
 )
 
 func main() {
-	// ANSI string with red text
-	colouredText := "\x1b[31mHello, World!\x1b[0m, this is a test"
-	godump.Dump(colouredText)
+	d := godump.NewDumper(godump.WithoutHeader())
+	colouredText := "\x1b[31mHello, World!\x1b[0m, this is a test. \x1b[33;45mThe end\x1b[0m."
+	fmt.Println(colouredText)
+	d.Dump(colouredText)
 
-	// Apply 50% fade
 	faded, err := tuifade.Fade(colouredText, 0.5)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
 	}
-
-	godump.Dump(faded)
 	fmt.Println(faded)
+	d.Dump(faded)
+
+	repeated := strings.Repeat(colouredText, 2)
+	fadedRepeated, err := tuifade.Fade(repeated, 0.5)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+	fmt.Println(fadedRepeated)
+	d.Dump(fadedRepeated)
+
 }
